46d9452f0318d121652e2c50db167d9043b40ade
venoooom21 <119972005+venoooom21@users.noreply.github.com>
2024-07-05 20:07:21 +0100

    server.js

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var http=require("http");
 http.createServer(function(request,response){
     response.writeHead(200,{'Content-Type':'text/html'});
     response.end('<h1>Hello Node!!!!</h1>\n');
-}).listen(3000)
+}).listen(3000,()=>{
+    console.log('Server runing at http://localhost:3000 ')
+})
 console.log('Server runing at http://localhost:3000 ')
